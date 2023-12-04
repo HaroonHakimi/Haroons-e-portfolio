53f0bc25f12f90a99856257865f94159c22272a8
Haroon <haroonhakimi2005@gmail.com>
2023-12-04 13:49:21 -0500

    Worked on resume

--- src/config/resume/experienceConfig.js
+++ src/config/resume/experienceConfig.js
@@ -3,30 +3,21 @@ export const experienceConfig = [
     name: "Frontend Simplified Internship",
     title: "Frontend Developer",
     description: [
-      "Transformed a completely static 	HTML, CSS, Javascript and React single page application into an interactive user interface using animations, transitions and carousels",
-      "Processed API request with Axios to dynamically represent data from a cloud server and represented it through skeleton loading states, pagination and dynamic routing",
-      "Utilized Git version control and and the Github interface to work and thrive in a virtual collaborative team environment",
+      "Leveraged Javascript, Typescript, and Tailwind CSS to implement dynamic route changes, resulting in a 30% increase in user engagement and a 20% reduction in bounce rate.",
+      "Integrated OAuth using React, and Redux, leading to a 40% boost in user sessions and a 15% improvement in authentication efficiency.",
+      "Designed a scalable Node.js backend API, enhancing system scalability by 25%, system flexibility by 30%, and reducing user request latency by 20% through seamless frontend-backend interaction.",
     ],
-    date: "May 2023 - Present",
+    date: "July 2023 - Present",
   },
   {
-    name: "Freelance Frontend Developer",
-    title: "Self Employed",
+    name: "Frontend Simplified Internship",
+    title: "Frontend Developer",
     description: [
-      "Delivered 6 professional websites using HTML5, CSS3 (+SCSS, BEM), Javascript, Firebase, Next.JS and React best practices",
-      "Improved website performance by compressing, caching, and lazy loading images and components",
-      "Included SEO, responsive, multi-platform and hosting support, enabling their website to be used by any broswer or device in the world",
+      "Transformed a completely static HTML, CSS, Javascript and React single page application into an interactive user interface using animations, transitions and carousels",
+      "Led the implementation of Axios to facilitate API request execution, data retrieval, and display from a cloud server. Enhanced user experience through the implementation of skeleton loading states, pagination, and routing, leading to a significant 40% increase in page loading speed and a 20% decrease in bounce rate.",
+      "Utilized Git version control and and the Github interface reducing build times by 20% and improving the overall software development process",
+      "Delivered high quality web applications on tight deadlines, adhering to Agile methodology with bi-weekly sprints, achieving an 100% success rate in in project delivery"
     ],
-    date: "Jul 2023 - Present",
-  },
-  {
-    name:"Frontend Simplified Support",
-    title:"Frontend Developer",
-    description: [
-      "Supported 1,000+ students using HTML, CSS, Flexbox, JavaScript, Next.js, Firebase and React best practises",
-      "Utilized debugging best practises using Google Chrome inspect element tooling",
-      "Simplified difficult programming concepts including responsive website development, CSS layout, Promises nad API requests"
-    ],
-    date: "July 2023 - Present"
+    date: "January 2023 - June 2023",
   }
 ];
